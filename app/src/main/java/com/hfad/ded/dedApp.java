2e1a6d9e7c9620609fd689560c8181e70570d938
Waleed Ashfaq <wqashfaq@uwaterloo.ca>
2020-04-04 17:47:47 -0400

    Application class added to store global constants and stuff

---
+++ app/src/main/java/com/hfad/ded/dedApp.java
@@ -0,0 +1,15 @@
+package com.hfad.ded;
+
+import android.app.Application;
+
+
+//This class instantiates when the application is first launched
+// and exists throughout the lifespan of the app
+public class dedApp extends Application {
+
+
+
+
+}
+
+
