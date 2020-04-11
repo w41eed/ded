de3c5a1d9a528dc1441b4a3ecc89e69001c2992c
Waleed Ashfaq <wqashfaq@uwaterloo.ca>
2020-04-10 21:27:48 -0400

    minor changes

--- app/src/main/java/com/hfad/ded/dedApp.java
+++ app/src/main/java/com/hfad/ded/dedApp.java
@@ -1,6 +1,9 @@
 package com.hfad.ded;
 
+import android.app.Activity;
 import android.app.Application;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
 
 
 //This class instantiates when the application is first launched
@@ -8,6 +11,12 @@ import android.app.Application;
 public class dedApp extends Application {
 
 
+    @Override
+    public void onCreate(){
+        super.onCreate();
+
+
+    }
 
 
 }
