1ae995183214171782c600d57be75552b4c83583
Waleed Ashfaq <wqashfaq@uwaterloo.ca>
2020-04-10 21:28:20 -0400

    moved logic methods to GameLogic class

--- app/src/main/java/com/hfad/ded/classicModeActivity.java
+++ app/src/main/java/com/hfad/ded/classicModeActivity.java
@@ -1,25 +1,11 @@
 package com.hfad.ded;
 
-import android.content.Context;
+
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import com.google.android.flexbox.FlexboxLayout;
-
-import org.w3c.dom.Text;
-
 import java.util.ArrayList;
-import java.util.Random;
-
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
+
 
 public class classicModeActivity extends AppCompatActivity {
 
@@ -30,6 +16,7 @@ public class classicModeActivity extends AppCompatActivity {
     private int score;
     private ArrayList<Integer> indexOfOccurence;
     private String letterChosen;
+    private GameLogic gameLogic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +27,9 @@ public class classicModeActivity extends AppCompatActivity {
         initVariables();
         getWordList();
 
+        gameLogic = new GameLogic(wordList, this, this);
 
-        createDash(5);
+
     }
 
 
@@ -72,37 +60,10 @@ public class classicModeActivity extends AppCompatActivity {
     }
 
 
-    //Checks If letter selected is in the current selected string
-    //if letter is present, adds on top of dash in corresponding indices
-    //if letter is absent then adds a strike
-    private ArrayList<Integer> whereIsLetterInWord(String letter, String word){
-
-        ArrayList<Integer> indices = new ArrayList<>();
-        for(int i=0; i<word.length(); i++){
-            //If character at i position of Word is equal to the letter
-            //which is charAt(0) to use equality sign
-            if(word.charAt(i) == letter.charAt(0)){
-                indices.add(i);
-            }
-        }
-        if(indices.isEmpty()){
-            //If indices is empty, it means no occurrence of letter
-            //so return null
-            return null;
-        } else {
-            //return list of indices where letter appears
-            return indices;
-        }
-    }
 
 
-    //returns random word from wordlist
-    private String randomWord(ArrayList<String> words){
-        Random rand = new Random();
-        int upperBound = words.size();
-        int randNum = rand.nextInt(upperBound);
-        return words.get(randNum);
-    }
+
+
 
 
     //sets up game for a new round
@@ -130,55 +91,20 @@ public class classicModeActivity extends AppCompatActivity {
     }
 
 
+
     //when user chooses a letter
     public void chooseLetter(View view){
 
-        //retrieves view Id in string format
-        letterChosen = getResources().getResourceEntryName(view.getId());
-
-        view.setClickable(false);
-
-
-        String correct = "correct";
-        String tag = String.valueOf(view.getTag());
-        correct = correct + tag;
-        int resId = getResources().getIdentifier(correct,"id", getPackageName());
-        ImageView correctView = findViewById(resId);
-        correctView.setVisibility(View.VISIBLE);
-
-        //FOR TESTING
-        TextView test = findViewById(R.id.test);
-        test.setText(letterChosen);
+        gameLogic.chooseALetter(view);
 
     }
 
 
-    //When replacing blank imageviw with correectly selected lewtter,
-    //get indices of occurrences of letter and replace letter at that spot
-    //To check if word is complete, subtract remaining length of string with length of occurence array
 
 
 
-    //Creates a linear layouyt with 2 imagviews in it
-    private void createDash(int lines){
-
-       ViewGroup cl = findViewById(R.id.classicLayout);
-
-       LayoutInflater inflater = getLayoutInflater();
-       FlexboxLayout fl =  cl.findViewById(R.id.dashLayout);//inflater.inflate(R.layout.letter_dashes_layout,null);
 
 
-        for (int i = 0; i < lines; i++) {
-            LinearLayout child = (LinearLayout) inflater.inflate(R.layout.dashes_and_letters, null);
-            ImageView let =  child.findViewById(R.id.correct_letter);
-            let.setImageResource(R.drawable.a);
-
-            if(fl != null){
-                fl.addView(child);
-            }
-
-        }
-    }
 
 
 }
