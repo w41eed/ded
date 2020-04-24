ed14babd2ab7416e2972df19263d4b598d6ccdd6
Waleed Ashfaq <wqashfaq@uwaterloo.ca>
2020-04-24 18:41:12 -0400

    dialog box now works

--- app/src/main/java/com/hfad/ded/GameLogic.java
+++ app/src/main/java/com/hfad/ded/GameLogic.java
@@ -1,11 +1,15 @@
 package com.hfad.ded;
 
+import android.animation.LayoutTransition;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.media.Image;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,32 +19,29 @@ import com.google.android.flexbox.FlexboxLayout;
 import java.util.ArrayList;
 import java.util.Random;
 
+import androidx.transition.Slide;
+import androidx.transition.Transition;
+
+
 public class GameLogic {
 
 
     private ArrayList<String> wordList;
     private ArrayList<Integer> indexOfOccurence;
-    private ArrayList<ImageView> dashLetters;
-    private ArrayList<ImageView> dashDashes;
+    private ArrayList<ImageView> dashLetters, dashDashes, correctsUsed, wrongsUsed;
     private ArrayList<View> lettersUsed;
-    private ArrayList<ImageView> correctsUsed;
-    private ArrayList<ImageView> wrongsUsed;
 
-    private String letterChosen;
     private String currWord;
 
-    private int wordLen;
-    private int currLen;
-    private int chancesLeft;
-    private int score;
-
-    private LinearLayout keyboard;
+    private int wordLen, currLen, chancesLeft, score;
 
     private Context context;
     private Activity activity;
 
     private resourcesClass resource;
 
+    private LinearLayout gameOver_dialog;
+    private LinearLayout win_dialog;
 
     public GameLogic(ArrayList<String> words, Context con, Activity act){
 
@@ -72,6 +73,22 @@ public class GameLogic {
         resource = new resourcesClass();
 
         setKeyboardClickable(true);
+
+        //set all listeners
+        setListeners();
+
+        //intialize score
+        score = 1;
+        setScore(score);
+
+
+        //get dialog id
+        gameOver_dialog = activity.findViewById(R.id.classic_game_over);
+        win_dialog = activity.findViewById(R.id.classic_win);
+        // move dialog down
+        slideDown(gameOver_dialog, 0);
+        slideDown(win_dialog, 0);
+
 
     }
 
@@ -167,7 +184,7 @@ public class GameLogic {
     public void chooseALetter(View view){
 
         //retrieves view Id in string format
-        letterChosen = context.getResources().getResourceEntryName(view.getId());
+        String letterChosen = context.getResources().getResourceEntryName(view.getId());
 
         //Letter is not clickable now
         view.setClickable(false);
@@ -191,8 +208,16 @@ public class GameLogic {
 
             currLen = currLen - indexOfOccurence.size();
             if(currLen == 0){
+
+                //show full word in green
                 showGreenWord();
-                resetGame();
+                //update score but dont display yet
+                score++;
+                //Make keyboard un clickable
+                setKeyboardClickable(false);
+                //show next round dialog
+                setCorrectWordVisibility(true);
+
             }
 
 
@@ -204,10 +229,12 @@ public class GameLogic {
 
             if(chancesLeft == 0){
 
+                //Show full word in red
                 showRedWord();
                 //Make keyboard un clickable
                 setKeyboardClickable(false);
                 //for testing
+                setGameOverVisibility(true);
                 //resetGame();
                 //gameOver();
             }
@@ -328,6 +355,10 @@ public class GameLogic {
 
     }
 
+
+    //toggles keyboard clickability
+    //turns off keyboard touch when user ends a round
+    //turns on keyboard touch at the start of the round
     private void setKeyboardClickable(boolean toggle){
 
         for(char keys='a'; keys <= 'z'; keys++){
@@ -344,6 +375,128 @@ public class GameLogic {
             }
         }
     }
+
+
+    //set all listeners
+    private void setListeners(){
+
+        //set Gamebar home button listener
+        ImageView homeButtonGameBar = activity.findViewById(R.id.go_home_gamebar);
+
+        homeButtonGameBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.startActivity(new Intent(context, HomeScreenActivity.class));
+            }
+        });
+
+
+        //set listener for home icon on game over dialog
+        ImageView homeButton_gameOver = activity.findViewById(R.id.go_home_wrong);
+
+        homeButton_gameOver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                context.startActivity(new Intent(context, HomeScreenActivity.class));
+
+                //make dialog invisible
+                setGameOverVisibility(false);
+            }
+        });
+
+
+        //set listener for play again icon on game over dialog
+        ImageView play_again = activity.findViewById(R.id.play_again);
+
+        play_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                score = 1;
+                setScore(score);
+                resetGame();
+
+                //make dialog invisible
+                setGameOverVisibility(false);
+            }
+        });
+
+        //set listener for next round icon on game over dialog
+        ImageView next_round = activity.findViewById(R.id.next_round);
+
+        next_round.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setScore(score);
+                resetGame();
+
+                //make dialog invisible
+                setCorrectWordVisibility(false);
+            }
+        });
+
+    }
+
+
+    //sets the score
+    private void setScore(int newScore){
+        TextView scoreView = activity.findViewById(R.id.current_score);
+        scoreView.setText(String.valueOf(newScore));
+    }
+
+
+    //Set visibility of win dialog
+    private void setCorrectWordVisibility(boolean visible){
+
+        if(visible){
+
+            win_dialog.setVisibility(View.VISIBLE);
+            slideUp(win_dialog, 500);
+
+        } else {
+            slideDown(win_dialog, 500);
+            win_dialog.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+    //Set visibility of gameOver dialog
+    private void setGameOverVisibility(boolean visible){
+
+        if(visible){
+            gameOver_dialog.setVisibility(View.VISIBLE);
+            slideUp(gameOver_dialog, 500);
+
+        } else {
+            slideDown(gameOver_dialog, 500);
+            gameOver_dialog.setVisibility(View.INVISIBLE);
+        }
+    }
+
+
+    // slide the view from below itself to the current position
+    public void slideUp(View view, int duration){
+        view.setVisibility(View.VISIBLE);
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                view.getHeight() *2,  // fromYDelta
+                0);                // toYDelta
+        animate.setDuration(duration);
+        view.startAnimation(animate);
+    }
+
+    // slide the view from its current position to below itself
+    public void slideDown(View view, int duration){
+        TranslateAnimation animate = new TranslateAnimation(
+                0,                 // fromXDelta
+                0,                 // toXDelta
+                0,                 // fromYDelta
+                view.getHeight() *2 ); // toYDelta
+        animate.setDuration(duration);
+        view.startAnimation(animate);
+    }
+
+
 
 
 }
