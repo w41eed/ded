e474e402bb8fac5521eb0e0e558169ed82b29cb0
Waleed Ashfaq <wqashfaq@uwaterloo.ca>
2020-04-26 16:51:43 -0400

    added getters and setters for setting values

--- app/src/main/java/com/hfad/ded/dedApp.java
+++ app/src/main/java/com/hfad/ded/dedApp.java
@@ -11,12 +11,75 @@ import android.os.Bundle;
 public class dedApp extends Application {
 
 
+    private String gender;
+    private String roundNum;
+    private String highScore;
+    private String keyboardStyle;
+    private boolean sound;
+
+
+
+
+
     @Override
     public void onCreate(){
         super.onCreate();
-
-
     }
+
+
+
+
+    //Get and set gender
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+
+    public String getGender(){
+        return gender;
+    }
+
+
+    //Get and set roundNum for when user reopens app
+    public void setRoundNum(String round){
+        roundNum = round;
+    }
+
+    public String getRoundNum(){
+        return roundNum;
+    }
+
+
+
+    //Get and set highscore
+    public void setHighScore(String score){
+        highScore = score;
+    }
+
+    public String getHighScore(){
+        return highScore;
+    }
+
+
+
+    //Get and set keyboard preference
+    public void setKeyboard(String keyboard){
+        this.keyboardStyle = keyboard;
+    }
+
+    public String getKeyboard(){
+        return keyboardStyle;
+    }
+
+
+    //get and set sound preference
+    public void setSound(boolean sound){
+        this.sound = sound;
+    }
+
+    public boolean getSound(){
+        return sound;
+    }
+
 
 
 }
